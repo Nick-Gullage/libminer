5ff955ccbbde2cb920943f524dec1d6a07331750
Nick Gullage <nickgullage@gmail.com>
2024-11-05 15:00:20 -0330

    added dependencies and tests

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,16 +2,32 @@
 #'
 #' A function description
 #'
+#' @param sizes LOGICAL; if TRUE (default), adds a column of the total bit size of libraries
+#'
 #' @return a data.frame of packages called
 #' @export
 #'
 #' @examples
-#' a use-case example
 #' lib_summary()
-lib_summary <-  function(){
+lib_summary <-  function(sizes = F){
+  if(!is.logical(sizes)){
+    stop("Sizes must be logical.")
+  }
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <-  as.data.frame(pkg_tbl, stringsAsFactors = F)
-  names(pkg_df) <- c("library", "n_packages")
+  names(pkg_df) <- c("Library", "n_packages")
+
+  if(sizes){
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      \(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = T)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
+
   pkg_df
 }
